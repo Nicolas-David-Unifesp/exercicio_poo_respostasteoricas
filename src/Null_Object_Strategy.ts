032fcb6e485037a9802ca7d526cd23ab9e15fbf5
Nicolas <nicolas.david@unifesp.br>
2026-06-26 19:00:18 -0300

    Fazendo o null object

--- src/Null_Object_Strategy.ts
+++ src/Null_Object_Strategy.ts
@@ -3,3 +3,19 @@
 /*Pelo que vi, basicamente, o padrão de projeto Null_Object é 
 quando há falta de estratégia de forma segura, ou seja, serve
 para eliminar os null/undefined no código principal. */
+
+import { DiaStrategy, Prioridade } from "./Strategy_Dia";
+
+export class Null_Object_Strategy implements DiaStrategy{
+    constructor(private readonly dialabel: string){}
+    // serve para que o código interno não altere o valor depois de criado
+
+    execute(_user: string, _extra: string): string {
+    return `Nenhuma estratégia definida para "${this.dialabel}". Nada a fazer.`;
+  }
+
+    //Basicamente, quero garantir que seja imutável e seguro  
+  getPriority(): Prioridade {
+    return Prioridade.BAIXA;
+  }
+}
